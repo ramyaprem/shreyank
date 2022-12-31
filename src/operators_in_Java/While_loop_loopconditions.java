f858ce6d8549f26471f69074d17989d72c46531a
premb <premb@DESKTOP-VU824D7>
2022-12-31 18:30:43 +1100

    commit

---
+++ src/operators_in_Java/While_loop_loopconditions.java
@@ -0,0 +1,25 @@
+package operators_in_Java;
+
+public class While_loop_loopconditions {
+
+	public static void main(String[] args) {
+		//program to display 1 to 5 using while loop
+		int n=1;
+		while (n<=5) {
+			System.out.println(n);
+			n++;
+			
+	}
+		System.out.println("demo for loop");
+		
+		//program to display from 5 to 1 using while loop
+		int N=5;
+		while (N>=1) {
+			System.out.println(N);
+			N--;
+		}
+		System.out.println("demo for while loop");
+		
+	}
+
+	}
